9ad08edb664f2c30866cb4d749e81c15b4d2cd35
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2022-10-21 21:57:28 +0530

    Added UserInfoViewset and LoginViewset

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from .views import LoginViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+router.register('login', LoginViewSet, basename='login')
+
+urlpatterns = [
+    path(r'', include(router.urls), name='users'),
+]
