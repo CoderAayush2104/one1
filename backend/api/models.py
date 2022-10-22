d42f73e5c24db9be054078e0e9c3a8792e0e6211
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2022-10-22 14:55:35 +0530

    Created Product, Category, Cart, CartItems Models and Migrated to DB

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
 # Create your models here.
@@ -24,3 +25,37 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f'{self.name} - {self.email}'
+
+class Category(models.Model):
+    name = models.CharField(_("category name"), max_length = 255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+        
+class Product(models.Model):
+    product_name = models.CharField(_("product name"), max_length = 255)
+    product_image = models.ImageField(default='default.jpg', upload_to='product_imgs')
+    product_desc = models.TextField(_("product description"))
+    product_price = models.DecimalField(_("product price"), decimal_places = 2, max_digits=8)
+    product_brand = models.CharField(_("product brand"), max_length = 255)
+    product_category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.product_name}"
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}'s cart"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cart}: {self.product}"
