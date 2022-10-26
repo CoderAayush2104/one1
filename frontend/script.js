ed653ea0994b59549dd75f73ceb86b066d09702f
shravanikaware <shravanikaware12@gmail.com>
2022-10-26 21:16:15 +0530

    added caurosel in home page

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $(".carousel .owl-carousel").owlCarousel({
+        autoplay: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items: 1,
+        smartSpeed: 300,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ]
+    });
+})
+
